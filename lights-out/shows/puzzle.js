d6c015c266f28fd2aabda39b4c4deea4ff1aec9f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-07-20 22:07:26 +0200

    Fail gracefully
    In stead of copying with an ugly error message an message is send
    to the user.

--- lights-out/shows/puzzle.js
+++ lights-out/shows/puzzle.js
@@ -3,11 +3,14 @@ function(doc, req) {
 	var Mustache = require("vendor/couchapp/lib/mustache");
 	var path = require("vendor/couchapp/lib/path").init(req);
 	
-	var context = {
-		"isDoc" : true,
-		assets : path.asset(),
-		numberOfStates : doc.numberOfStates,
-		problem :  JSON.stringify(doc.problem)
+	var context = { "isDoc" : false	};
+	if (doc && doc.type == 'description') {
+		context = {
+			"isDoc" : true,
+			assets : path.asset(),
+			numberOfStates : doc.numberOfStates,
+			problem :  JSON.stringify(doc.problem)
+		};
 	};
 
 	return Mustache.to_html(ddoc.template.lightsout, context, ddoc.template.partial);
