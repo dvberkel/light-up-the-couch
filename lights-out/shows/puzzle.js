65741fb488be42de0d0d899679edfd34ce05cc5f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-07-20 21:59:46 +0200

    Using description document
    Decription documents are used to create a puzzle.
    
    Note: this show does not fail gracely with no doc.

--- lights-out/shows/puzzle.js
+++ lights-out/shows/puzzle.js
@@ -2,9 +2,12 @@ function(doc, req) {
 	var ddoc = this;
 	var Mustache = require("vendor/couchapp/lib/mustache");
 	var path = require("vendor/couchapp/lib/path").init(req);
-
+	
 	var context = {
-		assets : path.asset()
+		"isDoc" : true,
+		assets : path.asset(),
+		numberOfStates : doc.numberOfStates,
+		problem :  JSON.stringify(doc.problem)
 	};
 
 	return Mustache.to_html(ddoc.template.lightsout, context, ddoc.template.partial);
