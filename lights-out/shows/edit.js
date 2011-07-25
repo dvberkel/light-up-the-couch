f431d72090e62bbc6bd7fb3b613cce3cc58eff88
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-07-25 18:25:55 +0200

    Added edit show
    The edit show demonstrates the use of the creation of descriptions.

---
+++ lights-out/shows/edit.js
@@ -0,0 +1,9 @@
+function(doc, req) {  
+	var ddoc = this;
+	var Mustache = require("vendor/couchapp/lib/mustache");
+	var path = require("vendor/couchapp/lib/path").init(req);
+	
+	var context = { assets : path.asset() };
+
+	return Mustache.to_html(ddoc.template.lightsoutEdit, context, ddoc.template.partial);
+}
