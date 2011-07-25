605e7b47e1db3eb35bf463e6e2697c7d00409ca7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-07-25 18:37:36 +0200

    Usin description
    The description is used to render the problem which then can be
    edit.
    
    Note: There is no means to submit the change back to couchdb

--- lights-out/shows/edit.js
+++ lights-out/shows/edit.js
@@ -3,7 +3,15 @@ function(doc, req) {
 	var Mustache = require("vendor/couchapp/lib/mustache");
 	var path = require("vendor/couchapp/lib/path").init(req);
 	
-	var context = { assets : path.asset() };
+	var context = { 
+		assets : path.asset(),
+		numberOfStates: 2,
+		problem: JSON.stringify([]) 
+	};
+	if (doc && doc.type == 'description') {
+		context['numberOfState'] = doc.numberOfStates;
+		context['problem'] = JSON.stringify(doc.problem);
+	}
 
 	return Mustache.to_html(ddoc.template.lightsoutEdit, context, ddoc.template.partial);
 }
